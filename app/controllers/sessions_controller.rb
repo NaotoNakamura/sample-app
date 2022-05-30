62e6188f69c10950bd58c4a892835a76d83b1cbb
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2022-05-30 21:57:01 +0900

    create login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      # remember user
+      redirect_to user
+    else
+      flash.now[:danger] = 'メールアドレスまたはパスワードが違います'
+      render 'new'
+    end
   end
 end
