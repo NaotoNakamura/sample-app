422a4662c9dd969e94ba6638bafb6a53c88d6da1
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2022-05-31 21:37:15 +0900

    add logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def logged_in?
     !current_user.nil?
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/index'
+  get 'posts/show'
   root 'sessions#new'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users, only: [:show]
 end
