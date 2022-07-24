835ee4d16126011f734b874c29d8f04b7f9d5668
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2022-07-24 16:21:55 +0900

    画像upload追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :body, presence: true, length: { maximum: 255 }
   default_scope -> { order(created_at: :desc) }
+  has_one_attached :picture
 end
